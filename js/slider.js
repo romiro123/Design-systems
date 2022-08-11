864c111dec1f1c497ca2f3bbb4ca445c075df1e5
Киленников Роман <kilennikovroman@gmail.com>
2022-08-11 11:42:54 +0300

    fix and new img

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,5 @@
-swiper = new Swiper(".swiper", { loop: !0, pagination: { el: ".swiper-pagination", clickable: !0 }, slideLabelMessage: !1 });
+swiper = new Swiper(".swiper--hero", {
+  loop: !0, allowTouchMove: false, autoplay: {
+    delay: 5000,
+  }, pagination: { el: ".swiper-pagination", clickable: !0 }, slideLabelMessage: !1
+});
